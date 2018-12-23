db599522a79b389f4aabc8667748883ab3d2037b
Franck Dechavanne <rallymen007@gmail.com>
2018-12-23 12:47:08 +0000

    wake me up inside

---
+++ GarfieldKartSaveEditor/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace GarfieldKartSaveEditor {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.locationPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.valueBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // locationPath
+            // 
+            this.locationPath.Location = new System.Drawing.Point(91, 10);
+            this.locationPath.Name = "locationPath";
+            this.locationPath.Size = new System.Drawing.Size(392, 20);
+            this.locationPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Save location";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(547, 303);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Options",
+            "Progression"});
+            this.comboBox1.Location = new System.Drawing.Point(42, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(279, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Value";
+            // 
+            // valueBox
+            // 
+            this.valueBox.Location = new System.Drawing.Point(57, 377);
+            this.valueBox.Name = "valueBox";
+            this.valueBox.Size = new System.Drawing.Size(426, 20);
+            this.valueBox.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Set Value";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 403);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(575, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(69, 17);
+            this.toolStripStatusLabel1.Text = "Not Loaded";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(102, 17);
+            this.toolStripStatusLabel2.Text = "No Items Selected";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel3.Text = "Not Set";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(61, 17);
+            this.toolStripStatusLabel4.Text = "Not Saved";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 425);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.valueBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.locationPath);
+            this.Name = "Form1";
+            this.Text = "Garfield Kart Save Editor";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox locationPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox valueBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+    }
+}
+
---
+++ GarfieldKartSaveLibrary/GarfieldKartSaveLoader.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GarfieldKartSaveLibrary {
+    public class GarfieldKartSaveLoader {
+        public GarfieldKartSave LoadSave(string path) {
+            GarfieldKartSave save = new GarfieldKartSave();
+
+            foreach(string filename in Directory.GetFiles(path)) {
+                if (filename.EndsWith("b3B0aW9ucw==")) {
+                    // Options file
+                    save.OptionsFilename = filename;
+                    save.Options = ParseOptionFile(filename);
+                    File.Copy(filename, filename + ".bak", true);
+                    continue;
+                }
+                if (filename.EndsWith("cHJvZ2Vzc2lvbg==")) {
+                    // Progression
+                    save.ProgressionFilename = filename;
+                    save.Progression = ParseProgressionFile(filename);
+                    File.Copy(filename, filename + ".bak", true);
+                    continue;
+                }
+            }
+
+            return save;
+        }
+
+        public void Save(GarfieldKartSave save) {
+            File.WriteAllLines(save.OptionsFilename, SerializeOptions(save));
+            File.WriteAllLines(save.ProgressionFilename, SerializeProgression(save));
+        }
+
+        public string[] SerializeOptions(GarfieldKartSave save) {
+            List<string> ret = new List<string>();
+            foreach(var opair in save.Options) {
+                ret.Add(Convert.ToBase64String(Encoding.UTF8.GetBytes(opair.Key))
+                    + ";"
+                    + Convert.ToBase64String(Encoding.UTF8.GetBytes(opair.Value)));
+            }
+            return ret.ToArray();
+        }
+
+        public string[] SerializeProgression(GarfieldKartSave save) {
+            List<string> ret = new List<string>();
+            foreach (var opair in save.Progression) {
+                ret.Add(Convert.ToBase64String(Encoding.UTF8.GetBytes(opair.Key))
+                    + ";"
+                    + Convert.ToBase64String(Encoding.UTF8.GetBytes(opair.Value)));
+            }
+            return ret.ToArray();
+        }
+
+        public Dictionary<string, string> ParseOptionFile(string filename) {
+            Dictionary<string, string> options = new Dictionary<string, string>();
+            foreach(string line in File.ReadAllLines(filename)) {
+                string[] split = line.Split(';');
+
+                byte[] data = Convert.FromBase64String(split[0]);
+                string param = Encoding.UTF8.GetString(data);
+                data = Convert.FromBase64String(split[1]);
+                string value = Encoding.UTF8.GetString(data);
+
+                options.Add(param, value);
+            }
+            return options;
+        }
+
+        public Dictionary<string, string> ParseProgressionFile(string filename) {
+            Dictionary<string, string> progression = new Dictionary<string, string>();
+            foreach (string line in File.ReadAllLines(filename)) {
+                string[] split = line.Split(';');
+
+                byte[] data = Convert.FromBase64String(split[0]);
+                string param = Encoding.UTF8.GetString(data);
+                data = Convert.FromBase64String(split[1]);
+                string value = Encoding.UTF8.GetString(data);
+
+                progression.Add(param, value);
+            }
+            return progression;
+        }
+    }
+
+    public class GarfieldKartSave {
+        public string OptionsFilename { get; set; }
+        public Dictionary<string, string> Options { get; set; }
+        public string ProgressionFilename { get; set; }
+        public Dictionary<string, string> Progression { get; set; }
+
+        public GarfieldKartSave() {
+            Options = new Dictionary<string, string>();
+            Progression = new Dictionary<string, string>();
+        }
+
+        public override string ToString() {
+            string str = "GarfieldKartSave::\nOptions:\n";
+            foreach(var opair in Options) {
+                str += opair.Key + "::" + opair.Value + "\n";
+            }
+            str += "Progression:\n";
+            foreach (var ppair in Progression) {
+                str += ppair.Key + "::" + ppair.Value + "\n";
+            }
+            str += "-----";
+            return str;
+        }
+    }
+}
